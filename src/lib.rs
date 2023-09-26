74d80ee1779655cd3a505b83800db9047f40d12e
developeruche <developeruche@gmail.com>
2023-09-26 23:28:54 +0100

    init: done interfacing

---
+++ src/lib.rs
@@ -0,0 +1,41 @@
+use ark_ff::{BigInteger, Field, One, PrimeField, Zero};
+
+
+
+
+
+
+
+struct KZG {
+
+}
+
+
+
+impl KZG {
+    pub fn new() {
+
+    }
+
+
+    pub fn trusted_set_up(&mut self) {
+
+    }
+
+    pub fn commit(&mut self) {
+
+    }
+
+    pub fn evaluate_n_proof(&mut self) {
+
+    }
+
+    pub fn verify_an_evaluation(&mut self) {
+
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+}
