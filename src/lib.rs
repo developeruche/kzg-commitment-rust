056ca89eb3cb22e1781614fa6f869b0954ca751d
developeruche <developeruche@gmail.com>
2023-09-27 02:36:06 +0100

    mod: done with the KZG struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,45 @@
-use ark_ff::{BigInteger, Field, One, PrimeField, Zero};
+mod playground;
+
+use ark_bls12_381::Bls12_381;
+use ark_ff::{BigInt, BigInteger, Field, One, PrimeField, Zero};
+use ark_poly::univariate::DensePolynomial;
+use ark_ec::pairing::Pairing;
+use ark_poly::DenseUVPolynomial;
+use ark_std::test_rng;
 
 
 
 
 
+type UniPoly_381 = DensePolynomial<<Bls12_381 as Pairing>::ScalarField>;
 
 
-struct KZG {
-
+#[derive(Default, Debug, Clone)]
+pub struct KZG {
+    pub polynomial: UniPoly_381,
+    // pub curve:
+    pub polynomial_degree: usize, // this would be operated on using BigInteger trait
+    pub tau: usize,
+    pub a: usize,
+    pub commitment: BigInt<{ usize }>,
+    pub quotient_polynomial: UniPoly_381,
+    pub proof_pie: BigInt<{ usize }>
 }
 
 
 
 impl KZG {
-    pub fn new() {
+    pub fn new() -> Self {
+        Self::default()
+    }
 
+    pub fn new_with_rand_poly() -> Self {
+        let rng = &mut test_rng();
+
+        Self {
+            polynomial: UniPoly_381::rand(10, rng),
+            ..Default::default()
+        }
     }
 
 
@@ -37,5 +62,14 @@ impl KZG {
 
 #[cfg(test)]
 mod tests {
+    use crate::*;
 
+
+    #[test]
+    pub fn test_kzg() {
+        let kzg = KZG::new_with_rand_poly();
+        let pt = kzg.clone().polynomial;
+
+        println!("I am printing now! pt _> {:?}", pt[0].to_string());
+    }
 }
